ed93f3b3deb645d91d1b38a5573e4556534594a7
クリホ <kuriho@users.noreply.github.com>
2023-05-31 14:14:18 +0200

    Update ytdlp.sh

--- .github/scripts/ytdlp.sh
+++ .github/scripts/ytdlp.sh
@@ -4,3 +4,6 @@ echo "GITHUB_REF: $Env:GITHUB_REF"
 echo "GITHUB_ACTOR: $Env:GITHUB_ACTOR"
 echo "GITHUB_REPOSITORY: $Env:GITHUB_REPOSITORY"
 git --version
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
