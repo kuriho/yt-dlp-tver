31675c8d03e990416881f907be5845ed1ed43a8a
クリホ <kuriho@users.noreply.github.com>
2023-05-31 15:31:50 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -3,7 +3,7 @@ set -e
 
 git clone --depth 1 --branch master --sparse --no-checkout https://github.com/yt-dlp/yt-dlp.git 
 # /tmp/yt_dlp/
-cd yt_dlp
+# cd yt_dlp
 git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
 git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py --no-cone
 
