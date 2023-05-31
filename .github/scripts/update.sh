8bd078351fa9917ed582ee2702c895c0cd7f4836
クリホ <kuriho@users.noreply.github.com>
2023-05-31 15:41:32 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 --branch master --sparse --no-checkout https://github.com/yt-dlp/yt-dlp.git /lib/yt_dlp/
+git clone --depth 1 --branch master --sparse --no-checkout https://github.com/yt-dlp/yt-dlp.git lib/yt_dlp/
 git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
 git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py --no-cone
 
-git add /lib/yt_dlp/
-
-#echo "we moving it"
-#ls /tmp/yt_dlp/
-#mv -f /tmp/yt_dlp/yt_dlp ${GITHUB_WORKSPACE}/lib/yt_dlp
+git add lib/yt_dlp/
 
 echo "we commit to it"
 git commit -m "auto update yt-dlp"
