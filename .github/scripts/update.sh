4f89adc7f40992bcadb1431a4205340c3687f461
クリホ <kuriho@users.noreply.github.com>
2023-05-31 16:27:01 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-ls -alrth 
-git clone --depth 1 --branch master --no-checkout https://github.com/yt-dlp/yt-dlp.git tmp/yt_dlp/
-ls -alrth 
-cd tmp/yt_dlp/
-git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
-git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py --no-cone
-ls -alrth
+git clone --depth 1 --branch master https://github.com/yt-dlp/yt-dlp.git tmp/yt_dlp/
 
-cd ..
-cd ..
-ls -alrth
+#ls -alrth 
+#git clone --depth 1 --branch master --no-checkout https://github.com/yt-dlp/yt-dlp.git tmp/yt_dlp/
+#ls -alrth 
+#cd tmp/yt_dlp/
+#git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
+#git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py --no-cone
+#ls -alrth
 
+#cd ..
+#cd ..
+#ls -alrth
+
+rm -r lib/yt_dlp/
 mv -f tmp/yt_dlp/* lib/yt_dlp/
 
 git add lib/yt_dlp/
