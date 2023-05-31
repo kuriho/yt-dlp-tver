2e906b737c140f0befe4f4b6ffcbe343f1528b39
クリホ <kuriho@users.noreply.github.com>
2023-05-31 02:19:11 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
+echo "clone it"
 git clone --depth 1 --branch master --no-checkout https://github.com/yt-dlp/yt-dlp.git /tmp/yt_dlp
 cd /tmp/yt_dlp
 git sparse-checkout set yt_dlp/* !yt_dlp/extractor/*
 git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py
+echo "we doing it"
 cd ..
 cd ..
+echo "we removing it"
 rm -r ${GITHUB_WORKSPACE}/lib/yt_dlp
+echo "we moving it"
 mv /tmp/yt_dlp/yt_dlp ${GITHUB_WORKSPACE}/lib/yt_dlp
 
+echo "we commit to it"
 git commit -m "[CI] auto update yt-dlp to upstream commit"
 echo "yt-dlp succesfully upgraded to upstream commit and staged for push"
