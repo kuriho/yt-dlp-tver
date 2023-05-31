ac3ed74bbd6591f4d0f35400218479e30b8adffe
クリホ <kuriho@users.noreply.github.com>
2023-05-31 14:18:19 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -8,6 +8,12 @@ echo "GITHUB_ACTOR: $Env:GITHUB_ACTOR"
 echo "GITHUB_REPOSITORY: $Env:GITHUB_REPOSITORY"
 git --version
 
+$authToken="x-access-token:${{ secrets.GITHUB_TOKEN }}"
+$bytes=[System.Text.Encoding]::UTF8.GetBytes($authToken)
+$encodedAuthToken=[Convert]::ToBase64String($bytes)
+          
+git config --local http.https://github.com/.extraheader "AUTHORIZATION: basic ${encodedAuthToken}"
+
 git clone --depth 1 --branch master --no-checkout https://github.com/yt-dlp/yt-dlp.git /tmp/yt_dlp/
 cd /tmp/yt_dlp/
 git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
