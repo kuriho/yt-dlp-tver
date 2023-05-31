b76864a1573b36caba697643bdb7084a28026d2e
クリホ <kuriho@users.noreply.github.com>
2023-05-31 15:52:16 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -9,6 +9,8 @@ git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
 git sparse-checkout add yt_dlp/extractor/brightcove.py yt_dlp/extractor/tver.py --no-cone
 ls -alrth
 
+mv -f ${GITHUB_WORKSPACE}/tmp/yt_dlp/ ${GITHUB_WORKSPACE}/lib/yt_dlp/
+
 git add lib/yt_dlp/
 
 echo "we commit to it"
