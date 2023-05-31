cada4d2f936dbb5efbf61e9b34b31a5a74df04dd
クリホ <kuriho@users.noreply.github.com>
2023-05-31 14:08:39 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-echo "clone it"
+
+echo "GITHUB_WORKSPACE: $Env:GITHUB_WORKSPACE"
+echo "GITHUB_SHA: $Env:GITHUB_SHA"
+echo "GITHUB_REF: $Env:GITHUB_REF"
+echo "GITHUB_ACTOR: $Env:GITHUB_ACTOR"
+echo "GITHUB_REPOSITORY: $Env:GITHUB_REPOSITORY"
+git --version
+
 git clone --depth 1 --branch master --no-checkout https://github.com/yt-dlp/yt-dlp.git /tmp/yt_dlp/
 cd /tmp/yt_dlp/
 git sparse-checkout set yt_dlp/* !yt_dlp/extractor/* --no-cone
