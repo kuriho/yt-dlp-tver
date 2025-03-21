fe92854aaf67c7754d73af1662a9ea143d70bf0b
クリホ <kuriho@users.noreply.github.com>
2025-03-22 00:35:36 +0100

    adjust unit test for new backend
    preparing for #2

--- test.py
+++ test.py
@@ -1,19 +1,34 @@
 import requests
+import os
 
 URL_BEST = f"https://service-api.tver.jp/api/v1/callEpisodeRankingDetail/variety"
 URL_VIDEO_WEBSITE = 'https://tver.jp/{}s/{}'
+
+def extract_first_manifest_url_from_info(result):
+    if 'manifest_url' in result:
+        return result['manifest_url']
+
+    if 'requested_formats' not in result:
+        return None
+    
+    for entry in result['requested_formats']:
+        if 'manifest_url' in entry and 'vcodec' in entry:
+            return entry['manifest_url']
+    return None
 
 def extract_info(url):
     from lib.yt_dlp import YoutubeDL
     from lib.yt_dlp.extractor.tver import TVerIE
 
+    proxy = os.environ.get('GEO_VERIFICATION_PROXY')
+
     ydl_opts = {
-        'format': 'bestvideo*+bestaudio/best'
+        'geo_verification_proxy': proxy
     }
 
-    ydl = YoutubeDL(ydl_opts)
-    ydl.add_info_extractor(TVerIE()) 
-    info = ydl.extract_info(url, download=False)
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.add_info_extractor(TVerIE()) 
+        info = ydl.extract_info(url, download=False)
     return info
 
 def get_valid_episode():
@@ -27,5 +42,7 @@ def test_tver():
     url = get_valid_episode()
     print(url)
     assert url
+
     info = extract_info(url)
-    assert info['manifest_url']
+    manifest_url = extract_first_manifest_url_from_info(info)
+    assert manifest_url
